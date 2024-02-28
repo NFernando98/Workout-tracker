e1c39472c6cee4c5e80e7323e3a7f8dc24418436
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2024-02-28 18:35:46 -0500

    Home page added few more things added on front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages & components
-import Home from "./pages/Home"
+import Home from './pages/Home'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar></Navbar>
       <div className="pages">
         <Routes>
-          <Route>
+          <Route 
             path="/"
-            element={<Home/>}
-          </Route>
+            element={<Home />}
+            /> 
         </Routes>
-
       </div>
-      </BrowserRouter>
-      
+      </BrowserRouter>      
     </div>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,36 @@
-const Home = () => {
-    return (
-        <div className="Home">
-            <h2>Home</h2>
-        </div>
-    )
-}
+import axios from 'axios';
+import { Key, useEffect, useState } from 'react';
 
-export default Home
+const Home = () => {
+    const [workouts, setWorkouts] = useState<{ _id: Key | null | undefined; title: string; }[] | null>(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/api/workouts');
+                if (response.status === 200) {
+                    setWorkouts(response.data.json);
+                    console.log(workouts)
+                } else {
+                    console.error('Failed to fetch workouts:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error fetching workouts:', error);
+            }
+        };
+
+        fetchWorkouts();
+    }, []);
+
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
