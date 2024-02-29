951a5a05fd7caa4d9099bd7611d704680227b94e
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2024-02-28 21:14:11 -0500

    component/WorkoutForm-finished-and-its-corresponding-css

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+
 
 const Home = () => {
     const [workouts, setWorkouts] = useState<any[]>([]);
@@ -32,6 +34,7 @@ const Home = () => {
                     <WorkoutDetails key={workout._id} workout={workout}  />
           ))}
             </div>
+            <WorkoutForm></WorkoutForm>
         </div>
     );
 };
