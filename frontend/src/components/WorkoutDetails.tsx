cbbfdb36e302fe0d443242b7bc139c6a8148581e
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2024-03-08 14:29:43 -0500

    date-package-used-to-format-date

--- frontend/src/components/WorkoutDetails.tsx
+++ frontend/src/components/WorkoutDetails.tsx
@@ -1,5 +1,8 @@
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
+// date fns
+import { formatDistanceToNow } from 'date-fns';
+
 const WorkoutDetails = ({ workout }: { workout: any }) => {
   const { dispatch } = useWorkoutsContext()
 
@@ -21,7 +24,7 @@ const WorkoutDetails = ({ workout }: { workout: any }) => {
       <h4>{workout.title}</h4>
       <p><strong>Load (kg): </strong>{workout.load}</p>
       <p><strong>Number of reps: </strong>{workout.reps}</p>
-      <p>{workout.createdAt}</p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
     </div>
   )
