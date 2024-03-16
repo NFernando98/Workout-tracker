20dae1256990f2032694fb434b674880f9bb6598
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2024-03-16 17:50:37 -0400

    feat(auth):
    - Take out workouts from screen when user logs out by redirecting them to log in page
    - When logging in, direct user to home page
    - In summary: conditionally show components based on user status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Navigate is used to redirect the user
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // pages & components
 import Home from './pages/Home'
@@ -7,27 +9,33 @@ import Signup from "./pages/Signup"
 import Navbar from './components/Navbar';
 
 function App() {
+  // User if used below to conditionally redirect to pages based on user object's status
+  // Element={user ? <Home/> : <Navigate to="/login"/>} means 
+  // if user is not null stay in Home, else goto login page
+
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar></Navbar>
-      <div className="pages">
-        <Routes>
-          <Route 
-            path="/"
-            element={<Home />}
-            /> 
-            <Route 
-            path="/login"
-            element={<Login />}
-            /> 
-            <Route 
-            path="/signup"
-            element={<Signup />}
-            /> 
-        </Routes>
-      </div>
-      </BrowserRouter>      
+        <Navbar></Navbar>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Home/> : <Navigate to="/login"/>}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login/> : <Navigate to="/"/>}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/"/>}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
