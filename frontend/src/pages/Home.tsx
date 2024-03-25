040a0a0dafffc6a13855e579869669fc6469d48c
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2024-03-25 14:53:31 -0400

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -25,7 +25,7 @@ const Home = () => {
                 };
 
                 // middleware will check config and if its valid it'll give access to this endpoint
-                const response = await axios.get('workout-tracker-api-iota.vercel.app', config);
+                const response = await axios.get('http://localhost:4000/api/workouts', config);
                 console.log('API Response:', response.data); // Log the API response
 
                 if (response.status === 200) {
