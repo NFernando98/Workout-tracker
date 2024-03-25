99108c3f35068f74f0cf9234e7c6699a96d8ca21
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2024-03-25 16:10:22 -0400

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -12,7 +12,13 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://workout-tracker-client-rho.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
